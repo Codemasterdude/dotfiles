b86f0cf65d3a14231d1a6f98e49d37e9f04e6899
dude <dudemmann@gmail.com>
2017-01-02 23:27:30 +0100

    add aliases.zsh and .zshrc files

---
+++ aliases.zsh
@@ -0,0 +1,8 @@
+# For a full list of active aliases, run `alias`.
+
+#Custom aliases:
+alias d='cd ~/Documents'
+alias dl='cd ~/Downloads'
+alias p='cd ~/Pictures'
+alias v='cd ~/Videos'
+alias dev='cd ~/Documents/Development'
