fa490e896323bc1903a31a275399c646086b8fa1
Codemasterdude <dudemmann@gmail.com>
2020-07-01 09:29:27 +0200

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,8 +1,14 @@
 # For a full list of active aliases, run `alias`.
 
 #Custom aliases:
-alias d='cd ~/Documents'
+alias doc='cd ~/Documents'
 alias dl='cd ~/Downloads'
 alias p='cd ~/Pictures'
-alias v='cd ~/Videos'
+alias vid='cd ~/Videos'
 alias dev='cd ~/Documents/Development'
+alias db='cd ~/Documents/Development/databases'
+alias biz='cd ~/Documents/Business'
+alias ebooks='cd ~/Documents/Ebooks'
+alias la='colorls -lAh --sd'
+alias ls='colorls --group-directories-first'
+alias lsd='colorls --group-directories-first -1'
